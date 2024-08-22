a8223a2ea779bbbb71dd953176e9cab5d642fee0
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2024-08-22 18:40:59 -0400

    output simple ppm image

---
+++ tinyray.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    // Image
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        for (int i = 0; i < image_width; i++) {
+            /*
+            Each rgb component is represented internally by 
+            real-valued variables that range from 0.01 to 1.0, 
+            so these must be scaled to integer values between 0 
+            and 255 before being print out.
+            */
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(2555.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
+
