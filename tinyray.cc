d72a7a80af0f100a0e3493c61577c8dbe27a4a96
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2024-08-23 00:02:05 -0400

    Add vec3 and color definitions and helper functions

--- tinyray.cc
+++ tinyray.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main() {
@@ -12,35 +15,29 @@ int main() {
         //Adds a progress indicator using the logging output stream
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            /*
-            Each rgb component is represented internally by 
-            real-valued variables that range from 0.01 to 1.0, 
-            so these must be scaled to integer values between 0 
-            and 255 before being print out.
-            */
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
+            //Calculate the colour of the pixel based on its position in the image
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(2555.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            //Write the pixel's colour out to the standard stream
+            write_color(std::cout, pixel_color);
         }
     }
 
+    //Notify rendering has completed
     std::clog << "\rDone.                 \n";
 }
 
 
 
 /*
-Build Command: g++ -o <desired program_name> <file>
-    ex: `g++ -o tinyray tinyray.cc`
+Build Command: 
+    - g++ -o <desired program_name> <file>
+        ex: `g++ -o tinyray tinyray.cc`
+    - g++ -std=c++11 -o tinyRay tinyray.cc 
+        - adds flag to use C++11 standard when compiling
 Run Command: ./<program name>
     ex: `./tinyray`
 Output Command: ./<program name> > <desired image name>.ppm
-    ex: `./tinryray < image.ppm`
+    ex: `./tinryray > image.ppm`
 */
 
