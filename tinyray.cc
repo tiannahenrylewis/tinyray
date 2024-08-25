9b49f1171d4e08c94d1cc8fb1ed36ace722d646f
Tianna Henry-Lewis <tianna_lewis05@hotmail.com>
2024-08-25 01:01:28 -0400

    adding Hittable objects

--- tinyray.cc
+++ tinyray.cc
@@ -1,32 +1,20 @@
+#include "rtweekend.h"
+
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
 
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (-half_b - sqrt(discriminant)) / a;
-    }
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
-        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1,1,1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5*(unit_direction.y() + 1.0);
+    auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
@@ -35,6 +23,11 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    //World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5, -1), 100));
 
     //Camera
     auto viewport_height = 2.0;
@@ -49,18 +42,19 @@ int main() {
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
+    for (int j = image_height-1; j >= 0; --j) {
         //Adds a progress indicator using the logging output stream
-        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
         for (int i = 0; i < image_width; i++) {
             auto u = double(i) / (image_width-1);
             auto v = double(j) / (image_height-1);
 
             //ray(origin, direction)
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
 
             //Calculate the colour of the pixel based on its position in the image
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
 
             //Write the pixel's colour out to the standard stream
             write_color(std::cout, pixel_color);
@@ -68,7 +62,7 @@ int main() {
     }
 
     //Notify rendering has completed
-    std::clog << "\rDone.                 \n";
+    std::clog << "\rDone.\n";
 }
 
 
